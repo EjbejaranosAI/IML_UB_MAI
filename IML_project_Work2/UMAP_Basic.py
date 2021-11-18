4d65930a6f8d17592b671d92e409158a2c454233
Ejbejaranos <edison.bejarano@estudiantat.upc.edu>
2021-11-18 10:04:59 +0100

    UMAP For reduction and UMAP for clustering. In my computer I couldn't run adult.arff because was out of ram. Maybe my computer

--- IML_project_Work2/UMAP_Basic.py
+++ IML_project_Work2/UMAP_Basic.py
@@ -1,24 +1,12 @@
 import numpy
-import numpy as np
-import sklearn
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
-from scipy.io import arff
 import umap
-
-
 from Projects.IML_UB_MAI.IML_project_Work2.utils.arff_parser import arff_to_df_normalized
 
-
-#data = 'datasets/vehicle.arff'
+data = 'datasets/vehicle.arff'
 #data = 'datasets/cmc.arff'
-data = 'datasets/adult.arff'   #Don't use this dataset yet, not is working
+#data = 'datasets/adult.arff'   #Don't use this dataset yet, not is working
 print(data)
-
 df_normalized, data_names, classes = arff_to_df_normalized(data)
 
 print(df_normalized.shape)
@@ -26,7 +14,6 @@ print(classes.shape)
 print(df_normalized.head)
 print(classes.head)
 reducer = umap.UMAP()
-
 print(data_names)
 
 embedding = reducer.fit_transform(df_normalized)
@@ -48,9 +35,9 @@ print('the color that we have are:',color)
 
 
 for i, Class in enumerate(set(classes)):
-    plt.scatter(embedding_normalize[:, 0][classes == Class],embedding_normalize[:, 1][classes == Class], cmap='Spectral', s=5, c = color[i])
+    plt.scatter(embedding_normalize[:, 0][classes == Class],embedding_normalize[:, 1][classes == Class], cmap='Spectral', s=0.8, c = color[i])
 plt.gca().set_aspect('equal', 'datalim')
-plt.suptitle(f'UMAP REDUCER FOR {data}')
+plt.suptitle('UMAP REDUCER FOR CMC DATASET')
 
 
 
@@ -63,5 +50,6 @@ maxElement_normalize = numpy.amax(embedding_normalize)
 
 print(maxElement)
 print(maxElement_normalize)
+
 
 
