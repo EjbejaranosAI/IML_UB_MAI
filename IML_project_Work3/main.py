a8a093e8066f69fe05f2af95cca387742d333be7
demetere <jr1kpoh2>
2021-12-05 13:12:51 +0100

    removed unnecessary comments from main

--- IML_project_Work3/main.py
+++ IML_project_Work3/main.py
@@ -4,18 +4,6 @@ from model_evaluator.model_evaluator import ModelEvaluator
 from utils.arff_parser import arff_to_df_normalized
 
 if __name__ == '__main__':
-    # tic = time.perf_counter()
-    # # data = 'datasets/vehicle.arff'
-    # data = 'datasetsCBR/bal/bal.fold.000000.train.arff'
-    #
-    # df_normalized, data_names, classes = arff_to_df_normalized(data)
-    #
-    # df_numpy = df_normalized.to_numpy()
-    #
-    # ib_class = IB('ib2')
-    # ib_class.fit_and_predict(df_numpy, classes)
-    # ib_class.print_results()
-
     model_evaluator = ModelEvaluator()
     model_evaluator.evaluate_model(algorithm='ib1', dataset_name='bal')
     model_evaluator.evaluate_model(algorithm='ib2', dataset_name='bal')
