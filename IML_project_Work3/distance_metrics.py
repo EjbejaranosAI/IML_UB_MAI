cd601526c2641a475d9a2aca857a1336b4239938
kubs0ne <kuba.glowacz@gmail.com>
2021-12-21 10:40:22 +0100

    corrected HVDM metric

--- IML_project_Work3/distance_metrics.py
+++ IML_project_Work3/distance_metrics.py
@@ -4,13 +4,22 @@ from math import sqrt
 
 def HVDM(array,datapoint):
     distances = []
-    for row in array:
+    if array.ndim>1:
+        for row in array:
+            s = []
+            for i, r in enumerate(row):
+                column = list(np.choose(i, array.T))
+                st = statistics.stdev(column)
+                subtract = abs(r-datapoint[i])
+                s.append((subtract/4*st)**2)
+            distances.append(sqrt(sum(s)))
+    else:
         s = []
-        for i, r in enumerate(row):
-            column = list(np.choose(i, array.T))
-            st = statistics.stdev(column)
-            subtract = abs(r-datapoint[i])
-            s.append((subtract/4*st)**2)
+        for i, r in enumerate(array):
+            subtract = abs(r - datapoint[i])
+            s.append(subtract)
         distances.append(sqrt(sum(s)))
+
     return distances
 
+print(HVDM(a,point))
